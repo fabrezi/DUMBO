e68367925b74f73f17695b93e9e278112dc93110
fabrezi <fkschon@gmail.com>
2022-12-05 15:48:32 -0500

    configure: Spring demo not working

---
+++ DUMBO/DUMBO/src/main/java/com/example/DUMBO/DumboApplication.java
@@ -0,0 +1,13 @@
+package com.example.DUMBO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumboApplication.class, args);
+	}
+
+}
